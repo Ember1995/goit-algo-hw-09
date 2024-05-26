00ee80a47241671037d1a56ad2a6d66f11953a81
Ember1995 <46302903+Ember1995@users.noreply.github.com>
2024-05-26 02:19:34 +0200

    file was updated

--- task_1_to_2.py
+++ task_1_to_2.py
@@ -34,23 +34,43 @@ def find_min_coins(amount):
 
     return result
 
+# Тестування
 if __name__ == "__main__":
-    
+
+    # Задана умовами задачі сума
     amount = 113
 
     # Жадібний алгоритм
-    start_time = time.time()
-    greedy_result = find_coins_greedy(amount)
-    greedy_time = time.time() - start_time
+    start_time_1 = time.time()
+    greedy_result_1 = find_coins_greedy(amount)
+    greedy_time_1 = time.time() - start_time_1
 
     # Алгоритм динамічного програмування
-    start_time = time.time()
-    dp_result = find_min_coins(amount)
-    dp_time = time.time() - start_time
+    start_time_1 = time.time()
+    dp_result_1 = find_min_coins(amount)
+    dp_time_1 = time.time() - start_time_1
 
-    print("\nТаблиця результатів:")
-    print("| Параметр                 | Жадібний алгоритм          | Динамічне програмування    |")
-    print("|--------------------------|----------------------------|----------------------------|")
-    print(f"| Час виконання (сек)      | {greedy_time:.10f}               | {dp_time:.10f}               |")
-    print(f"| Очікуваний результат     | {{50: 2, 10: 1, 2: 1, 1: 1}} | {{1: 1, 2: 1, 10: 1, 50: 2}} |")
-    print(f"| Отриманий результат      | {greedy_result} | {dp_result} |")
+    print("\nПеревірка результату: ")
+    print("| Параметр                  | Жадібний алгоритм          | Динамічне програмування      |")
+    print("|---------------------------|----------------------------|------------------------------|")
+    print(f"| Очікуваний результат      | {{50: 2, 10: 1, 2: 1, 1: 1}} | {{1: 1, 2: 1, 10: 1, 50: 2}}   |")
+    print(f"| Отриманий результат       | {greedy_result_1} | {dp_result_1}   |")
+
+    # Збільшуємо суму
+    amount = 1113
+
+    # Жадібний алгоритм
+    start_time_2 = time.time()
+    greedy_result_2 = find_coins_greedy(amount)
+    greedy_time_2 = time.time() - start_time_2
+
+    # Алгоритм динамічного програмування
+    start_time_2 = time.time()
+    dp_result_2 = find_min_coins(amount)
+    dp_time_2 = time.time() - start_time_2
+
+    print("\nТестування різних сум:")
+    print("| Сума                      | Жадібний алгоритм, сек     | Динамічне програмування, сек |")
+    print("|---------------------------|----------------------------|------------------------------|")
+    print(f"| 113                       | {greedy_time_1:.10f}               | {dp_time_1:.10f}                 |")
+    print(f"| 1113                      | {greedy_time_2:.10f}               | {dp_time_2:.10f}                 |")
